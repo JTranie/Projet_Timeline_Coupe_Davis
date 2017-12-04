839ce3905fbc9b5f266a451fd27e4d06c190cbb4
JTranie <julien.tranie@sciencespo.fr>
2017-12-04 15:17:05 +0100

    Rendu Final

---
+++ pape-timeline.js
@@ -0,0 +1,168 @@
+var timeline_json = {
+    "title": {
+        "media": {
+          "url": "https://i.eurosport.com/2017/02/02/2017040-42336287-2560-1440.jpg?w=1700",
+          "caption": "Le saladier d'argent, trophé de la coupe Davis",
+          "credit": ""
+        },
+        "text": {
+          "headline": "Grande et petites histoires de la Coupe Davis<br/> 1900 - 2017",
+          "text": ""
+        }
+    },
+    "events": [
+      {
+        "media": {
+          "url": "https://upload.wikimedia.org/wikipedia/commons/b/b9/D.F.Davis_and_H.Ward.The_American_Team_that_Played_Against_England_in_Doubles.1900.jpg",
+          "caption": "Les frères Dwight Davis lors de la première épreuve.",
+          "credit": ""
+        },
+        "start_date": {
+          "year": "1900"
+        },
+        "text": {
+          "headline": "Création de la coupe",
+          "text": "En 1899, Dwight Davis (1879-1945), un étudiant américain de l'Université Harvard, l'un des meilleurs joueurs de tennis du moment, a l'idée de créer une compétition annuelle internationale mettant aux prises les États-Unis et la Grande-Bretagne. La première coupe Davis se déroule à Boston, Massachusetts, sur les courts en gazon du Longwood Cricket Club, du 7 au 10 août 1900"
+        }
+      },
+      {
+        "media": {
+          "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Davis_Cup_Praha_%C4%8CRo_2012-11-28_cropped_1.jpg/800px-Davis_Cup_Praha_%C4%8CRo_2012-11-28_cropped_1.jpg",
+          "caption": "",
+          "credit": "<a href=\"http://unidiscmusic.com\">Unidisc Music</a>"
+        },
+        "start_date": {
+          "year": "1902"
+        },
+        "text": {
+          "headline": "Un saladier à 1000 $",
+          "text": "Alors que la compétition n'a pas eu lieu en 1901, elle reprend vie en 1902. C'est alors que les frères Dwight Davis décide d'offrir comme récompense le fameux saladier d'argent. A partir de ce jour, chaque équipe gagnante verra gravée son nom sur la coupe."
+        }
+      },
+      {
+        "media": {
+          "url": "https://www.francetvinfo.fr/image/750luek21-cecb/578/325/4715849.jpg",
+          "caption": "Le joueur américain Bill Tilden (G) serre la main du Français Henri Cochet, lors d'un match de Coupe Davis disputé à Germantown (Etats-Unis), en 1926.",
+          "credit": "AFP"
+        },
+        "start_date": {
+          "year": "1920"
+        },
+        "end_date": {
+          "year": "1926"
+        }, 
+        "text": {
+          "headline": "Premier règne Américain",
+          "text": "Les États-Unis détiennent le record du plus grand nombre de titres : sur 61 finales disputées, ils en ont remporté 32, dont sept consécutives de 1920 à 1926. Ils sont les seuls avec la France à avoir remporté la Coupe Davis sur toutes les surfaces (gazon, terre battue, synthétique et dur)"
+        }
+      },
+      {
+        "media": {
+          "url": "https://i1.wp.com/www.memosport.fr/wp-content/uploads/2011/06/rene-lacoste.jpg",
+          "caption": "Le champion de tennis français René Lacoste",
+          "credit": ""
+        },
+        "start_date": {
+          "year": "1927"
+        },
+        "end_date": {
+          "year": "1932"
+        }, 
+        "text": {
+          "headline": "Règne des 4 Mousquetaires français",
+          "text": "De 1927 à 1932, l'équipe française, composée de René Lacoste, Henri Cochet, Jean Borotra et Jacques Brugnon (les « Quatre Mousquetaires »), domine la compétition avec six victoires consécutives."
+        }
+      },
+      {
+        "media": {
+          "url": "http://www.gqmagazine.fr/uploads/images/201622/cb/rod_laver_interview_6194.jpeg",
+          "caption": "L'équipe d'Australie victorieuse en 1961.",
+          "credit": ""
+        },
+        "start_date": {
+          "year": "1950"
+        },
+        "end_date": {
+          "year": "1967"
+        }, 
+        "text": {
+          "headline": "La période des Australiens",
+          "text": "17 ans et 14 victoires en autant de temps. L'équipe d'Australie fût entraînée par plusieurs générations de chanmpion avec notamment Rod Laver, à partir de 1959."
+        }
+      },
+      {
+        "media": {
+          "url": "https://i.eurosport.com/2010/11/28/665436-21712093-2560-1440.jpg?w=700",
+          "caption": "TENNIS COUPE DAVIS 1982 France-USA Noah Leconte McEnroe Fleming - AFP",
+          "credit": ""
+        },
+        "start_date": {
+          "year": "1982"
+        },
+        "text": {
+          "headline": "John McEnroe l'invincible",
+          "text": "En 1982, McEnroe a gagné douze matchs sur douze, huit simples et quatre doubles. Seul cas dans l'Histoire."
+        }
+      },
+      {
+        "media": {
+          "url": "http://www.memosport.fr/wp-content/uploads/2010/12/coupe-davis-2001.jpg",
+          "caption": "2001 : Escudé et la France remportent la Coupe Davis",
+          "credit": ""
+        },
+        "start_date": {
+          "year": "2001"
+        },
+        "text": {
+          "headline": "Victoire Française d'une génération dorée",
+          "text": "Au terme d'une rencontre au couteau contre l'Australie (3-2), les joueurs français, emmenés par le capitaine Guy Forget, on remporté la coupe, sur le gazon de Melbourne. Ce fût un grand moment de la carrière de joueurs célèbres comme Fabrice Santoro, Cédric Pioline, Sébastien Grosjean ou encore Nicolas Escudé et Arnaud Clément."
+        }
+      },
+      {
+        "media": {
+          "url": "http://www.letelegramme.fr/ar/imgproxy.php/images/2014/11/20/paul-henri-mathieu-en-blanc-inconsolable-dans-les-bras-de_2156458.jpg?article=20141120-1010432241&aaaammjj=20141120",
+          "caption": "Paul-Henri Mathieu (en blanc) inconsolable dans les bras de Fabrice Santoro après sa défaite en finale contre la Russie, le 1er décembre 2002 à Bercy.",
+          "credit": ""
+        },
+        "start_date": {
+          "year": "2002"
+        },
+        "text": {
+          "headline": "Le terrible renversement",
+          "text": "Après avoir gagné la coupe en 2001, c'est la Russie qui gagne la Coupe Davis en France lors du cinquième match en 2002, grâce à une victoire en cinq manches de Mikhail Youzhny sur Paul-Henri Mathieu après avoir été mené deux manches à zéro. C'est la seule fois où un pays s'est imposé sur un tel renversement dans la dernière rencontre."
+        }
+      },
+      {
+        "media": {
+          "url": "https://i.eurosport.com/2013/02/02/947765-19630833-2560-1440.jpg?w=700",
+          "caption": "",
+          "credit": ""
+        },
+        "start_date": {
+          "day": "",
+          "month": "",
+          "year": "2013"
+        },
+        "text": {
+          "headline": "Le match le plus long",
+          "text": "Le match le plus long de l'histoire de la Coupe Davis est le double opposant la paire suisse Stanislas Wawrinka - Marco Chiudinelli aux Tchèques Tomáš Berdych et Lukáš Rosol lors du premier tour. Le duo tchèque s'impose après 7 h 2 min de match, sur le score de 4-6, 7-5, 4-6, 7-6, 22-24."
+        }
+      },
+      {
+        "media": {
+          "url": "https://static.lexpress.fr/medias_11685/w_640,h_360,c_fill,g_north/v1511775131/l-equipe-de-france-de-coupe-davis-fete-sa-victoire-en-finale-aux-depens-de-la-belgique-le-26-novembre-2017-a-villeneuve-d-ascq-pres-de-lille_5983200.jpg",
+          "caption": "L'ensemble de l'équipe de France, titulaires et remplaçants tenant le saladier d'argent.",
+          "credit": "AFP"
+        },
+        "start_date": {
+          "month": "",
+          "day": "",
+          "year": "2017"
+        },
+        "text": {
+          "headline": "Victoire après 16 ans de disette",
+          "text": "Dernière victoire en date française avec le quatuor : Jo wilfried tsonga, Lucas Pouille, Richard Gasquet, Pierre-Hugues Herbert. Avec un capitaine désormais mythique : Yannick Noah."
+        }
+      }
+    ]
+};
